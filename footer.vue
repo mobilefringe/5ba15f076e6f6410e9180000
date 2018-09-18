6d4a075a7616855c0b8f364a535dd67e95547a82
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-18 16:34:16 -0400

    Orchid Template Revised 1.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer v-if="footerBanner" v-bind:style="{ backgroundImage: 'url(' + footerBanner.image_url + ')' }">
+    <footer>
 		<div class="footer">
 			<div class="site_container">
 			    <div class="row footer_logo">
@@ -10,26 +10,7 @@
 				            </router-link>
 				        </div>    
 			        </div>
-			        <div class="col-xs-12 visible_phone">
-			            <div class="row">
-			                <div class="col-xs-4">
-			                    <div class="language_select">
-					                <span @click="changeLocale('en-ca')">EN</span> | <span @click="changeLocale('fr-ca')">FR</span>
-				                </div>    
-			                </div>
-			                <div class="col-xs-8">
-			                    <div class="header_search_container">
-                			        <search-component v-model="search" :list="processedStores" :suggestion-attribute="suggestionAttribute" :placeholder="$t('header.search')" @select="onOptionSelect">
-                                        <template slot="item" scope="option">
-                                            <article class="media">
-                                                <p>{{ option.data.name }}</p>
-                                            </article>
-                                        </template>
-                                    </search-component>
-                                </div>
-			                </div>
-			            </div>
-			        </div>
+			        
 			        <div class="col-md-6">
 			            <nav id="footer_nav">
     						<ul>
@@ -58,64 +39,27 @@
 </template>
 
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue!search-component"], function (Vue, Vuex, moment, tz, VueMoment, SearchComponent) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function (Vue, Vuex, moment, tz, VueMoment) {
         return Vue.component("footer-component", {
             template: template, // the variable template will be injected,
             data: function data() {
                 return {
-                    footerBanner: null,
-                    suggestionAttribute: 'name',
-                    search: '',
-                    newsletter_email: "",
+
                 }
             },
             props:['footer_menu_items', 'social_media'],
-            created() {
-                this.loadData().then(response => {
-                    this.dataLoaded = true;
-                    
-                    var temp_repo = this.findRepoByName('Footer Banner');
-                    if(temp_repo) {
-                        this.footerBanner = temp_repo.images[0];
-                    }
-                });
-            },
+            // created() {
+            //     this.loadData().then(response => {
+            //         this.dataLoaded = true;
+
+            //     });
+            // },
             computed: {
                 ...Vuex.mapGetters([
                     'property',
-                    'findRepoByName',
-                    'processedStores'
                 ]),
-                locale: {
-                    get () {
-                        return this.$store.state.locale
-                    },
-                    set (value) {
-                        this.$store.commit('SET_LOCALE', { lang: value })
-                    }
-                },
                 copyright_year() {
                     return moment().year();
-                }
-            },
-            methods: {
-                loadData: async function() {
-                    try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
-                    } catch (e) {
-                        console.log("Error loading data: " + e.message);
-                    }
-                },
-                changeLocale: function(val) {
-                    // this will update the data store, which in turn will trigger the watcher to update the locale in the system
-                    this.locale = val; 
-                },
-                onOptionSelect(option) {
-                    console.log('Selected option:', option);
-                    this.$nextTick(function() {
-                        this.search = ""
-                    });
-                    this.$router.push("/stores/" + option.slug);
                 }
             }
         });
