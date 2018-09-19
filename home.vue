a2311df775495a81a26dfabc4dc126d7401a2d70
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-19 14:57:30 -0400

    Orchid Template Revised 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -20,7 +20,7 @@
         		<div class="site_container">
         		    <div class="home_page_title_container">
         		        <div class="stores_header_line"></div>
-        		        <h3 class="home_page_title caps">{{ $t("home_page.explore") }} {{ property.name }}</h3>
+        		        <h3 class="home_page_title caps">What's Happening</h3>
         		        <div class="stores_header_line"></div>
         		    </div>
         		    <div v-masonry transition-duration="0.3s" item-selector=".grid-item" class="hidden_phone">
