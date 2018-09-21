e0d7b204d9b1d62b9d4eaa5aa16bd6f5c6e3ac18
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-21 13:31:11 -0400

    Orchid Template Revised 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -17,7 +17,7 @@
                             <div class="details_store_name">
                                 <h2>{{ currentStore.name }}</h2>
                                 <p class="store_category">{{ storeCategory }}</p>
-                                <h3 class="store_phone">{{ currentStore.phone }}</h3>
+                                <h3 class="store_phone">{{ currentStore.phone }} <i class="fa fa-phone"></i></h3>
                                 <div class="">
                                     <div class="store_website animated_btn">Visit Website</div>
                                     <div class="store_website animated_btn">View Map</div>
