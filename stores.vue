12d8dbca46762a62daa191bb35d37b248fc3d1da
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-24 15:39:37 -0400

    Orchid Template Revised 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -81,7 +81,7 @@
                     dataLoaded: false,
                     pageBanner : null,
                     search_result : null,
-                    shopFilter: 6439
+                    storeFilter: 6439
                 }
             },
             created (){
