2d180f59e61be946e473bb2a5d6a5f1320304534
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-24 16:37:49 -0400

    Orchid Template Revised 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -120,8 +120,8 @@
                     var categories = [];
                     _.forEach(store_categories, function(value, key) {
                         console.log(value)
-                        category = vm.findCategoryById(value);
-                        categories.push(category)
+                        // category = vm.findCategoryById(value);
+                        // categories.push(category)
                     });
                     
                     console.log(categories)
