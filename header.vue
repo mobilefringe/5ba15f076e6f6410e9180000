7feb4fda187ff981b2c5f2c5d6ad957a04719cbe
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-24 15:29:56 -0400

    Orchid Template Revised 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -83,6 +83,7 @@
     							    <div class="social_icons center">
                                         <span v-for="item in social_media">
                                             <a :href="item.url" target="_blank">
+                                                <p class="accessibility">{{ item.name }}</p>
                                                 <i :class="item.iconClass" aria-hidden="true"></i>
                                             </a>
                                         </span>
