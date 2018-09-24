985925f8a6baf947d7410cb38a6b5442e3e63797
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-24 11:15:43 -0400

    Orchid Template Revised 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -48,7 +48,7 @@
                                     </div>
                                     <div class="col-sm-12 col-md-6">
                                         <div class="position_relative" :class="{'has-error': errors.has('message')}">
-                                            <label class="label" for="message">Message</label>
+                                            <label class="label" for="message">Message*</label>
                                             <textarea v-model="form_data.message" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="message" type="text" :placeholder="$t('contact_page.form_message')" data-vv-delay="500"></textarea>
                                             <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
                                         </div>
