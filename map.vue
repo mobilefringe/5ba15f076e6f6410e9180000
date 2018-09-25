eb0b48a327f95b1ca1fc8a88baeaa57e61c874d3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-25 17:27:30 -0400

    Orchid Template Revised 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -31,7 +31,7 @@
                         <div class="margin-30 visible-mobile"></div>
                         <div class="col-md-3 col-md-pull-9">
                             <div class="store_list_container hidden-mobile">
-                                <div  v-if="filteredStores">
+                                <div v-if="filteredStores">
                                     <div class="store_name" v-for="store in filteredStores">
                                         <p v-on:click="addLandmark(store)">{{store.name}}</p>
                                     </div>
