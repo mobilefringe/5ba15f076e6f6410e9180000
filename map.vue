57bb898db638ba65269bc4d81c8482622e1ae894
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-25 16:54:42 -0400

    Orchid Template Revised 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -11,16 +11,6 @@
         			</div>
         		</div>  
                 <div class="site_container">
-                    <!--<div class="row">-->
-                    <!--    <div class="col-md-12">-->
-                    <!--        <div class="alpha_list hidden_phone">-->
-                    <!--            <a @click="filterStores('All')" class="all_a">All</a>-->
-                    <!--            <a @click="filterStores('#')">#</a>-->
-                    <!--            <a v-for="letter in alphabet" @click="filterStores(letter)">{{letter}}</a>-->
-                    <!--        </div>-->
-                    <!--        <div class="margin_40"></div>-->
-                    <!--    </div>-->
-                    <!--</div>-->
                     <div class="row">
                         <div class="col-md-9 col-md-push-3">
                             <png-map ref="pngmap_ref" :png-map-url="getPNGurl" :initial-position="'700 450'" @updateMap="updatePNGMap"></png-map>
@@ -70,9 +60,6 @@
                 return {
                     dataLoaded: false,
                     pageBanner: null,
-                    // alphabet : [
-                    //     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-                    // ],
                     suggestionAttribute: "name",
                     storeSearch: null,
                     filteredStores: null,
