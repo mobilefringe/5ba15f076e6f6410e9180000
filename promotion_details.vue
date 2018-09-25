43dd872903012cb43714583640fafcdd2884af04
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-25 13:06:25 -0400

    Orchid Template Revised 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -38,12 +38,10 @@
     					<!--    </div>-->
     					<!--    <div class="promo_img" v-if="locale=='en-ca'" v-lazy:background-image="currentPromo.image_url"></div>-->
     					<!--    <div class="promo_img" v-else v-lazy:background-image="currentPromo.promo_image2_url_abs"></div>-->
-    					<!--    <div class="promo_details_desc">-->
-         <!--   				    <div v-if="locale=='en-ca'" v-html="currentPromo.rich_description"></div>-->
-         <!--   				    <div v-else v-html="currentPromo.rich_description_2"></div>-->
-         <!--   				</div>-->
-    					<!--</div>-->
-        	<!--		</div>-->
+				    <div class="promo_details_desc">
+    				    <div v-if="locale=='en-ca'" v-html="currentPromo.rich_description"></div>
+    				    <div v-else v-html="currentPromo.rich_description_2"></div>
+    				</div>
 		        </div>
 		    </div>
 		</transition>
