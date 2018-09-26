986d30883b538f789a741499be3a4a6e0d52ef8c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-26 13:14:14 -0400

    Orchid Template Revised 1.0  /scoop.vue

--- scoop.vue
+++ scoop.vue
@@ -42,7 +42,7 @@
 
 <script>
     define(["Vue", "vuex"], function(Vue, Vuex) {
-        return Vue.component("kids-club-component", {
+        return Vue.component("scoop-component", {
             template: template, // the variable template will be injected
             data: function() {
                 return {
