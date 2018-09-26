b23baca41bb6fe285b7db39564440a81f7b3a7b9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-26 10:45:34 -0400

    Orchid Template Revised 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -68,7 +68,7 @@
             },
             created(){
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Promotions Banner');
+                    var temp_repo = this.findRepoByName('Events Banner');
                     if (temp_repo) {
                         try {
                             this.pageBanner = temp_repo.images[0];
