3dc403b4bf67a35a7e4da94de46f61809aff8596
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-08 18:24:26 -0400

    Orchid Template Revised 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -44,12 +44,12 @@
 					<div class="hidden_phone">
 					    <div class="header_search_container">
     					    <search-component v-if="headerReady" :list="searchList" placeholder="Search" :suggestion-attribute="suggestionAttribute" :keys="keys" v-model="search_result" @select="onOptionSelect" :autocomplete="false" :minMatchCharLength="3" :tokenize="true" class="text-left">
-                                    <template slot="item" scope="option" class="manual">
-                                        <article class="media">
-                                            <p>{{ option.data.name }}</p>
-                                        </article>
-                                    </template>
-                                </search-component>
+                                <template slot="item" scope="option" class="manual">
+                                    <article class="media">
+                                        <p>{{ option.data.name }}</p>
+                                    </article>
+                                </template>
+                            </search-component>
                         </div>
 					</div>
 				</div>
