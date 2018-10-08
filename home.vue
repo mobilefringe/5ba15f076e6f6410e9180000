f0c5324f0e1a0a788ca07d596996ad9b250bac0c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-08 16:47:34 -0400

    Orchid Template Revised 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -32,7 +32,7 @@
                             				<div class="feature_item_content">
                             					<p>{{ feature.name }}</p>
                             					<h3>{{ feature.description }}</h3>
-                            					<div class="feature_item_more">View Details</div>
+                            					<div class="feature_item_more" :aria-label="feature.name">View Details</div>
                             				</div>
                             			</div>
                             		</a>
@@ -56,7 +56,7 @@
                             				<div class="feature_item_content">
                             					<p>{{ feature.name }}</p>
                             					<h3>{{ feature.description }}</h3>
-                            					<div class="feature_item_more">View Details</div>
+                            					<div class="feature_item_more" :aria-label="feature.name">View Details</div>
                             				</div>
                             			</div>
                             		</a>
