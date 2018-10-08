c27e8d96067655fb7fde2f170d35d35959cddda2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-08 19:33:28 -0400

    Orchid Template Revised 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -18,6 +18,12 @@
         			</div>
         		</div>
         		<div class="site_container">
+        		    <div class="show_phone">
+        		        <div class="home_page_title_container">
+            		        <h5 class="home_page_subtitle center caps">Discover {{ property.name }}</h5>
+            		        <h3 class="home_page_title caps">Find Your Store</h3>
+            		    </div>
+        		    </div>
         		    <div class="home_page_title_container">
         		        <h5 class="home_page_subtitle center caps">Discover {{ property.name }}</h5>
         		        <h3 class="home_page_title caps">What's Happening</h3>
