ae6cf7b9cf98a0d01864074a0c29188cf1f20bce
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-08 19:28:12 -0400

    Orchid Template Revised 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -94,7 +94,6 @@
                 },
                 getSVGMap() {
                     var mapURL = "https://www.mallmaverick.com" + this.property.svgmap_url.split("?")[0];
-                    console.log(mapURL)
                     return mapURL
                 },
                 floorList() {
