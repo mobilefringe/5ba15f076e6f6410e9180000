610e792d7f2482bf20a0de95236dbfec940da5f2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-09 14:55:35 -0400

    Orchid Template Revised 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -187,7 +187,7 @@
                 programs () {
                     var features = _.slice(this.$store.state.feature_items, 6, 8); 
                     _.forEach(features, function(value, key) {
-                        if ( _.includes([7], key) ) {
+                        if ( _.includes(value.name, "Kids Club")) {
                             value.program_class = "last";
                         }
                     });
