b101110e8b4ef4b00ab8c35b11c561bd47c1a83c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-10 17:23:06 -0400

    Orchid Template Revised 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -283,7 +283,6 @@
                     });
 
                     var list = _.union( stores, events, promos, jobs );
-                    console.log("list", list)
                     return list
                 },
             },
