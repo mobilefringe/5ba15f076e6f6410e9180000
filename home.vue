d844b5c7837b4ffdaca82d9593f0974886f8bc2a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-10 17:18:51 -0400

    Orchid Template Revised 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -21,7 +21,7 @@
         		    <div class="visible_phone">
         		        <div class="homepage_hours">
         		            <h5 class="home_page_subtitle center caps" v-if="hour.is_open" v-for="hour in todaysHours">Open Now:</h5>
-        		            <h5 class="home_page_subtitle center caps" v-else>{{ property.name }} is currently</h5>
+        		            <h5 class="home_page_subtitle center caps" v-else>{{ property.name }} is</h5>
                             <h3 class="home_page_title caps" v-if="hour.is_open" v-for="hour in todaysHours">{{hour.open_time | moment("h a", timezone)}} - {{hour.close_time | moment("h a", timezone)}}</h3>
                             <h3 class="home_page_title caps" v-else>Closed</h3>
                         </div>
